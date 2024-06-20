68c0411347a08e2e4d4ea903f32f782bb6736c41
NegarEsfandyari <149689602+esfandnegar@users.noreply.github.com>
2024-06-20 04:59:52 -0700

    delete 4 line

--- exp8.py
+++ exp8.py
@@ -6,17 +6,16 @@ while True:
     try: 
         html = urlopen('http://coinranking.com/') 
         bs = BeautifulSoup(html.read(),'lxml') 
-        #print(bs.body.div.div.div) 
+         
         table_rows= bs.find_all('div',{'class':'valuta'}) 
-        #print(table_rows[0:3:2]) 
+         
         for i in range(0,10,2): 
             print(f"{i}===========================") 
-            #print(table_rows[i]) 
-            #print(table_rows[i].get_text()) 
+         
             price_string = table_rows[i].get_text().replace('$','').replace(" " ,"").replace("\n","").replace("," ,"") 
             price = float(price_string) 
             print(price) 
-            print("**") 
+            print("******") 
     except URLError as e: 
         print('you should connect ot internt') 
         sleep(1)  
